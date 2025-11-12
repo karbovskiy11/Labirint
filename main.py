5fa9c0bcb82c41ab6ed62512c3b8c30e0af12ec9
«karbovskiy11» <karbovskiy11@gmail.com>
2025-11-12 19:19:15 +0400

    request and response, create file and reade file

---
+++ main.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://www.labirint.ru/genres/1852/'
+
+headers = {
+    'accept': '*/*',
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36'
+}
+
+# request = requests.get(url, headers=headers)
+# response = request.text
+#
+# with open('index.html', 'w', encoding='utf-8') as file:
+#     file.write(response)
+
+with open('index.html', 'r', encoding='utf-8') as file:
+    index = file.read()
+
+soup = BeautifulSoup(index, 'lxml')
+all_category = soup.find('div', class_='js-content-block-tab').find_all('div', class_='genres-carousel__item')
+
+for item in all_category:
+    item_titles = item.find('div', class_='product').get('data-name')
+
