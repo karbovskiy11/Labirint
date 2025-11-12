443eaf75efd48a472593559723735a26dcf8f29f
karbovskiy11 <karbovskiy11@gmail.com>
2025-11-12 23:41:08 +0400

    Collecting data from the page

--- main.py
+++ main.py
@@ -20,8 +20,37 @@ with open('index.html', 'r', encoding='utf-8') as file:
 soup = BeautifulSoup(index, 'lxml')
 all_category = soup.find('div', class_='js-content-block-tab').find_all('div', class_='genres-carousel__item')
 
+all_books = []
 for item in all_category:
     item_titles = item.find('div', class_='product').get('data-name')
+    item_price = int(item.find('div', class_='product').get('data-price'))
+    item_discount_price = int(item.find('div', class_='product').get('data-discount-price'))
+    item_sale = str(round(item_discount_price / item_price * 100)) + '%'
+    item_author = item.find('div', class_='product').find('div', class_='product-author').find('a').get('title')
+    item_pubhouse = (item.find('div', class_='product').find('div', class_='product-pubhouse').
+                     find('a',class_='product-pubhouse__pubhouse').get('title'))
+    try:
+        item_series = ': ' + (item.find('div', class_='product').find('div', class_='product-pubhouse').
+        find_next('a',class_='product-pubhouse__series').get(
+            'title'))
+    except:
+        item_series = ''
+
+    item_pubhouse_series = item_pubhouse + item_series
+
+    all_books.append(
+        (
+            item_titles,
+            item_price,
+            item_discount_price,
+            item_sale,
+            item_author,
+            item_pubhouse_series
+        )
+    )
+
+print(all_books)
+
 
 
 
