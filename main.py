03d36fa69876c86d0adf192d198b6a740d8d777a
karbovskiy11 <karbovskiy11@gmail.com>
2025-11-14 21:04:04 +0400

    update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os.path
+
 from bs4 import BeautifulSoup
 import requests
 import csv
@@ -9,11 +11,26 @@ headers = {
     'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36'
 }
 
-# request = requests.get(url, headers=headers)
-# response = request.text
+request = requests.get(url, headers=headers)
+response = request.text
 #
 # with open('index.html', 'w', encoding='utf-8') as file:
 #     file.write(response)
+
+# with open('index.html', 'r', encoding='utf-8') as file:
+#     index = file.read()
+
+soup = BeautifulSoup(response, 'lxml')
+all_category = soup.find('div', class_='js-content-block-tab').find_all('div', class_='genres-carousel__item')
+count_page = int(
+    soup.find('div', class_='pagination-number__right').find('div', class_='pagination-number').find_next('a').text)
+numeration = 1
+
+if os.path.exists('pages') is not True:
+    os.mkdir('pages')
+
+if os.path.exists('data') is not True:
+    os.mkdir('data')
 
 with open('labirint_books.csv', 'w', newline='', encoding='utf-8') as csvfile:
     csvwriter = csv.writer(csvfile, delimiter=',')
@@ -29,51 +46,56 @@ with open('labirint_books.csv', 'w', newline='', encoding='utf-8') as csvfile:
         )
     )
 
-with open('index.html', 'r', encoding='utf-8') as file:
-    index = file.read()
+for page in range(1, count_page + 1):
 
-soup = BeautifulSoup(index, 'lxml')
-all_category = soup.find('div', class_='js-content-block-tab').find_all('div', class_='genres-carousel__item')
+    url = f'https://www.labirint.ru/genres/1852/?page={page}'
+    headers = {
+        'accept': '*/*',
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36'
+    }
 
-all_books = []
-numeration = 1
-cout_page = soup.find('div', class_='pagination-number__right').find('div', class_='pagination-number').find_next('a').text
+    request = requests.get(url, headers=headers)
+    response = request.text
 
+    with open(f'pages/page_{page}.html', 'w', encoding='utf-8') as link:
+        link.write(response)
 
-for item in all_category:
-    item_titles = item.find('div', class_='product').get('data-name')
-    item_price = int(item.find('div', class_='product').get('data-price'))
-    item_discount_price = int(item.find('div', class_='product').get('data-discount-price'))
-    item_sale = str(round(item_discount_price / item_price * 100)) + '%'
-    item_author = item.find('div', class_='product').find('div', class_='product-author').find('a').get('title')
-    item_pubhouse = (item.find('div', class_='product').find('div', class_='product-pubhouse').
-                     find('a',class_='product-pubhouse__pubhouse').get('title'))
-    try:
-        item_series = ': ' + (item.find('div', class_='product').find('div', class_='product-pubhouse').find('a',class_='product-pubhouse__series').get('title'))
-    except:
-        item_series = ''
+    soup = BeautifulSoup(response, 'lxml')
+    all_category = soup.find('div', class_='js-content-block-tab').find_all('div', class_='genres-carousel__item')
 
-    item_pubhouse_series = item_pubhouse + item_series
+    for item in all_category:
+        item_titles = item.find('div', class_='product').get('data-name')
+        item_price = int(item.find('div', class_='product').get('data-price'))
+        item_discount_price = int(item.find('div', class_='product').get('data-discount-price'))
+        item_sale = str(round(item_discount_price / item_price * 100)) + '%'
+        try:
+            item_author = item.find('div', class_='product').find('div', class_='product-author').find('a').get('title')
+        except:
+            item_author = 'Автор отсутствует'
 
-    with open('labirint_books.csv', 'a', newline='', encoding='utf-8') as csvfile:
-        csvwriter = csv.writer(csvfile, delimiter=',')
-        csvwriter.writerow(
-            (
-                numeration,
-                item_titles,
-                item_author,
-                item_pubhouse_series,
-                item_price,
-                item_discount_price,
-                item_sale
+        item_pubhouse = (item.find('div', class_='product').find('div', class_='product-pubhouse').
+                         find('a', class_='product-pubhouse__pubhouse').get('title'))
+        try:
+            item_series = ': ' + (item.find('div', class_='product')
+                .find('div', class_='product-pubhouse').find('a',class_='product-pubhouse__series').get('title'))
+        except:
+            item_series = ''
+
+        item_pubhouse_series = item_pubhouse + item_series
+
+        with open(f'data/labirint_books_{page}.csv', 'a', newline='', encoding='utf-8') as csvfile:
+            csvwriter = csv.writer(csvfile, delimiter=',')
+            csvwriter.writerow(
+                (
+                    numeration,
+                    item_titles,
+                    item_author,
+                    item_pubhouse_series,
+                    item_price,
+                    item_discount_price,
+                    item_sale
+                )
             )
-        )
-    numeration += 1
+        numeration += 1
 
-# print(cout_page)
-
-
-
-
-
-
+    print(f'Page {page} completed')
