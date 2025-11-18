6d5e102734fa9a8145b13539b58788b49f7e6663
karbovskiy11 <karbovskiy11@gmail.com>
2025-11-18 21:17:59 +0400

    create def write_data_books

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 import os.path
-from test import headers, response
 
 def get_headers():
     headers = {
@@ -60,81 +59,70 @@ def main():
     soup = BeautifulSoup(first_page_content, 'lxml')
     count_page = get_count_paginations(soup)
     all_category = soup.find('div', class_='js-content-block-tab').find_all('div', class_='genres-carousel__item')
+    data_books(all_category)
 
-
-    for page in range(1, 2):
-
-        # url = f'https://www.labirint.ru/genres/1852/?page={page}'
-        # headers = {
-        #     'accept': '*/*',
-        #     'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36'
-        # }
-        #
-        # request = requests.get(url, headers=headers)
-        # response = request.text
-        #
-        # with open(f'pages/page_{page}.html', 'w', encoding='utf-8') as link:
-        #     link.write(response)
-
-
-
+def data_books(all_category):
         # собираем данные о книге
-        for item in all_category:
-            product = item.find('div', class_='product')
-            if not product:
-                continue    # пропустить элемент, если нет основного блока
+    data = []
+    numeration = 1
+    for item in all_category:
+        product = item.find('div', class_='product')
+        if not product:
+            continue    # пропустить элемент, если нет основного блока
 
-            # название книги
-            item_titles = product.get('data-name')
-            if not item_titles:
-                item_titles = 'Название не указано'
+        # название книги
+        item_titles = product.get('data-name')
+        if not item_titles:
+            item_titles = 'Название не указано'
 
-            # цена книги (начальная, со скидкой, размер скидки)
-            try:
-                item_price = int(product.get('data-price', 0))
-                item_discount_price = int(product.get('data-discount-price', 0))
-                if item_price > 0:
-                    item_sale = f'{round(item_discount_price / item_price * 100)}%'
-                else:
-                    item_sale = '0%'
-            except(ValueError, TypeError):
-                item_price = 0
-                item_discount_price = 0
+        # цена книги (начальная, со скидкой, размер скидки)
+        try:
+            item_price = int(product.get('data-price', 0))
+            item_discount_price = int(product.get('data-discount-price', 0))
+            if item_price > 0:
+                item_sale = f'{round(item_discount_price / item_price * 100)}%'
+            else:
                 item_sale = '0%'
+        except(ValueError, TypeError):
+            item_price = 0
+            item_discount_price = 0
+            item_sale = '0%'
 
-            # автор книги
-            item_author = product.find('div', class_='product-author').find('a').get('title')
-            if not item_author:
-                item_author = 'Автор не указан'
+        # автор книги
+        item_author = product.find('div', class_='product-author').find('a').get('title')
+        if not item_author:
+            item_author = 'Автор не указан'
 
-            # издательство и издательская серя книги
-            product_pubhouse = product.find('div', class_='product-pubhouse')
-            item_pubhouse = product_pubhouse.find('a', class_='product-pubhouse__pubhouse').get('title')
-            if not item_pubhouse:
-                item_pubhouse = 'Идательство не указано'
-            try:
-                item_series = ': ' + product_pubhouse.find('a',class_='product-pubhouse__series').get('title')
-            except(AttributeError):
-                item_series = ''
-            item_pubhouse_series = item_pubhouse + item_series
+        # издательство и издательская серя книги
+        product_pubhouse = product.find('div', class_='product-pubhouse')
+        item_pubhouse = product_pubhouse.find('a', class_='product-pubhouse__pubhouse').get('title')
+        if not item_pubhouse:
+            item_pubhouse = 'Идательство не указано'
+        try:
+            item_series = ': ' + product_pubhouse.find('a',class_='product-pubhouse__series').get('title')
+        except(AttributeError):
+            item_series = ''
+        item_pubhouse_series = item_pubhouse + item_series
+        data.append(
+            (
+                numeration,
+                item_titles,
+                item_author,
+                item_pubhouse_series,
+                item_price,
+                item_discount_price,
+                item_sale
+            )
+        )
+        numeration += 1
+    write_data_books(data)
 
-            #
-            with open(f'data/labirint_books_{page}.csv', 'a', newline='', encoding='utf-8') as csvfile:
-                csvwriter = csv.writer(csvfile, delimiter=',')
-                csvwriter.writerow(
-                    (
-                        numeration,
-                        item_titles,
-                        item_author,
-                        item_pubhouse_series,
-                        item_price,
-                        item_discount_price,
-                        item_sale
-                    )
-                )
-            numeration += 1
+def write_data_books(data):          #
+    with open(f'data/labirint_books.csv', 'a', newline='', encoding='utf-8') as csvfile:
+        csvwriter = csv.writer(csvfile, delimiter=',')
+        csvwriter.writerows(data)
 
-        print(f'Page {page} completed')
+print(f'Page completed')
 
 if __name__ == "__main__":
     main()
